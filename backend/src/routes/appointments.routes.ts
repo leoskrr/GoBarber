ac61fcf2c19afb2dbacd84ff3e3a3e85140041ab
Leonardo Viana <sonleo2013@gmail.com>
2020-06-24 17:17:40 -0400

    rewritten with typeorm imports

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,35 +1,28 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
-
-// Rota: receber a requisição, chamar outro arquivo para tratar essa requisição, devolver uma resposta
-
-// SoC: Separation of Concerns (Separação de preocupaçôes)
-
-// lógica/regra de negócio -> dentro de services (eles são executados na rota)
 
 appointmentsRouter.get('/', (request, response) => {
-    const appointments = appointmentsRepository.all();
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = appointmentsRepository.find();
 
     return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
     try {
         const { provider, date } = request.body;
 
         const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService(
-            appointmentsRepository,
-        );
+        const createAppointment = new CreateAppointmentService();
 
-        const appointment = createAppointment.execute({
+        const appointment = await createAppointment.execute({
             date: parsedDate,
             provider,
         });

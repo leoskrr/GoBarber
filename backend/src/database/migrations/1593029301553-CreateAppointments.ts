961bbcbadc97c71e905c5f5181f716891ac89a2d
Leonardo Viana <sonleo2013@gmail.com>
2020-06-24 16:19:25 -0400

    table appointments formatted inside it's migration

--- backend/src/database/migrations/1593029301553-CreateAppointments.ts
+++ backend/src/database/migrations/1593029301553-CreateAppointments.ts
@@ -1,7 +1,34 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1593029301553 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {}
+export default class CreateAppointments1593029301553
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {}
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
+    }
 }

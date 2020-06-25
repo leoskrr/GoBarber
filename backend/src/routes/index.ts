5cd74ab11818dcfd829a0edf6494b68d393ae1fa
Leonardo Viana <sonleo2013@gmail.com>
2020-06-25 19:12:38 -0400

    created user session

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import appointmentRouter from './appointments.routes';
 import userRouter from './user.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;

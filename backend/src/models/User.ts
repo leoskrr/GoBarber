da3e17122c1090b54ff95f547eed1df9088bd491
Leonardo Viana <sonleo2013@gmail.com>
2020-06-25 16:54:18 -0400

    created user's routes file and create operation

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -6,7 +6,7 @@ import {
     UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
+@Entity('users')
 class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appointmentRouter from './appointments.routes';
+import userRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
+routes.use('/users', userRouter);
 
 export default routes;

78ee68fb42bf3ec60b2766d0a4f026613efffbd4
Leonardo Viana <sonleo2013@gmail.com>
2020-06-24 22:26:02 -0400

    created user migration and table

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -4,26 +4,21 @@ import {
     PrimaryGeneratedColumn,
     CreateDateColumn,
     UpdateDateColumn,
-    ManyToOne,
-    JoinColumn,
 } from 'typeorm';
 
-import User from './User';
-
 @Entity('appointments')
-class Appointment {
+class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider_id: string;
+    name: string;
 
-    @ManyToOne(() => User)
-    @JoinColumn({ name: 'provider_id' })
-    provider: User;
+    @Column()
+    email: string;
 
-    @Column('timestamp with time zone')
-    date: Date;
+    @Column()
+    password: string;
 
     @CreateDateColumn()
     created_at: Date;
@@ -32,4 +27,4 @@ class Appointment {
     updated_at: Date;
 }
 
-export default Appointment;
+export default User;

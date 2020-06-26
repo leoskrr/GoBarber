94bbe7ac07552928788a5ef1b4d7757a3cf36c0b
Leonardo Viana <sonleo2013@gmail.com>
2020-06-25 22:36:59 -0400

    done service for updating avatar

---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,47 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+    user_id: string;
+    avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+    public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne(user_id);
+
+        if (!user) {
+            throw new Error('User is not authenticated');
+        }
+
+        if (user.avatar) {
+            // Delete latest avatar
+
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+            const userAvatarFileExists = await fs.promises.stat(
+                userAvatarFilePath,
+            );
+            if (userAvatarFileExists) {
+                // delete file
+                await fs.promises.unlink(userAvatarFilePath);
+            }
+        }
+
+        user.avatar = avatarFilename;
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateUserAvatarService;

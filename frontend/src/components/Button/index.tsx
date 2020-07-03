910a3d292c31fc2c299525637aaea36bb2710f7c
Leonardo Viana <sonleo2013@gmail.com>
2020-07-03 14:28:21 -0400

    created components folder

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button: React.FC = () => <button type="button">Teste</button>;
+
+export default Button;
---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Input: React.FC = () => <input type="text" />;
+
+export default Input;

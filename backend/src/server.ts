696f4b102b1864bd2cf8e4847cc68f5f73d480f8
Leonardo Viana <sonleo2013@gmail.com>
2020-07-05 14:00:43 -0400

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -12,6 +13,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);

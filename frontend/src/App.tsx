c8b6688bbabf5c2e8ab6c95bb7af107917153046
Leonardo Viana <sonleo2013@gmail.com>
2020-07-05 20:54:00 -0400

    created toasts containers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
-
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -11,6 +11,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );

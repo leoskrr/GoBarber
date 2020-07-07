daa09104bb6862db9126799a88edb972e91c1773
Leonardo Viana <sonleo2013@gmail.com>
2020-07-06 23:02:19 -0400

    removing toast automatically after 3s

---
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+
+import { ToastMessage, useToast } from '../../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+}
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, removeToast]);
+
+  return (
+    <Container type={message.type} hasDescription={!!message.description}>
+      <FiAlertCircle size={20} />
+
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button onClick={() => removeToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,35 +1,19 @@
 import React, { useCallback } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { ToastMessage, useToast } from '../../hooks/toast';
-import { Container, Toast } from './styles';
+import Toast from './Toast';
+
+import { ToastMessage } from '../../hooks/toast';
+import { Container } from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button onClick={() => removeToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );

99ac7dd50f0647acc2fba0e9da38fd7457e3264c
Leonardo Viana <sonleo2013@gmail.com>
2020-07-06 23:06:55 -0400

    added icons for each type of toast

--- frontend/src/components/ToastContainer/Toast/index.tsx
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
@@ -8,6 +13,12 @@ import { Container } from './styles';
 interface ToastProps {
   message: ToastMessage;
 }
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
@@ -24,7 +35,7 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
 
   return (
     <Container type={message.type} hasDescription={!!message.description}>
-      <FiAlertCircle size={20} />
+      {icons[message.type || 'info']}
 
       <div>
         <strong>{message.title}</strong>

9e8fe3f454bc96e914779eb0e08ab6ab8d35740d
Leonardo Viana <sonleo2013@gmail.com>
2020-07-09 15:17:39 -0400

    created dashboard page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,10 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+
+    <Route path="/dashboard" exact component={Dashboard} />
   </Switch>
 );
 

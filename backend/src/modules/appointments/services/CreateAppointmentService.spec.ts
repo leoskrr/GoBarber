f560bf56c9f9e1a57b6e0abf4d8a05702e4c305e
Leonardo Viana <sonleo2013@gmail.com>
2020-07-17 23:30:24 -0400

    created test for creating appointments

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test('sum two numbers', () => {
-    expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointments', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '21232435',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('21232435');
+    });
+
+    // it('should not be able to create to appontments on the same time', () => {
+    //     expect().toBe();
+    // });
 });

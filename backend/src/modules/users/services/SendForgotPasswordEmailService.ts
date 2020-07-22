201781fd51b45069204539b031434c15a946d436
Leonardo Viana <sonleo2013@gmail.com>
2020-07-21 22:56:23 -0400

    created tests for sending forgot password

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,36 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+    email: string;
+}
+@injectable()
+class SendForgotPasswordEmailService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('MailProvider')
+        private mailProvider: IMailProvider,
+    ) {}
+
+    public async execute({ email }: IRequest): Promise<void> {
+        const checkUserExists = await this.usersRepository.findByEmail(email);
+
+        if (!checkUserExists) {
+            throw new AppError('User does not exists.');
+        }
+
+        this.mailProvider.sendMail(
+            email,
+            'Pedido de recuperação de senha recebido',
+        );
+    }
+}
+
+export default SendForgotPasswordEmailService;

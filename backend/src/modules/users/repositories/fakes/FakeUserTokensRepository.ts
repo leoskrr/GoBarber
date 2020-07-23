c3f62ae2cfffd0ed55fb2b0a4c3ba5ba88a4df50
Leonardo Viana <sonleo2013@gmail.com>
2020-07-23 16:50:59 -0400

    created all tests for resetting password

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -15,6 +15,8 @@ class FakeUserTokensRepository implements IUserTokensRepository {
             id: uuid(),
             token: uuid(),
             user_id,
+            created_at: new Date(),
+            updated_at: new Date(),
         });
 
         this.userTokens.push(userToken);
--- backend/src/modules/users/services/ResetPasswordService.ts
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -1,10 +1,10 @@
 import { injectable, inject } from 'tsyringe';
+import { isAfter, addHours } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
-
-// import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     token: string;
@@ -18,6 +18,9 @@ class ResetPasswordService {
 
         @inject('UserTokensRepository')
         private userTokensRepository: IUserTokensRepository,
+
+        @inject('IHashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ token, password }: IRequest): Promise<void> {
@@ -32,7 +35,14 @@ class ResetPasswordService {
             throw new AppError('User does not exists');
         }
 
-        user.password = password;
+        const tokenCreatedAt = userToken.created_at;
+        const compareDate = addHours(tokenCreatedAt, 2);
+
+        if (isAfter(Date.now(), compareDate)) {
+            throw new AppError('Token expired');
+        }
+
+        user.password = await this.hashProvider.generateHash(password);
 
         await this.usersRepository.save(user);
     }

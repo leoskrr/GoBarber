0c1027904e6de75b656b363cc95af4ce4fb558ee
Leonardo Viana <sonleo2013@gmail.com>
2020-07-28 23:36:08 -0400

    created hbs template for sending emails

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,11 +1,10 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
-
-// import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
     email: string;
@@ -32,6 +31,13 @@ class SendForgotPasswordEmailService {
 
         const { token } = await this.userTokensRepository.generate(user.id);
 
+        const forgotPasswordTemplate = path.resolve(
+            __dirname,
+            '..',
+            'views',
+            'forgot_password.hbs',
+        );
+
         await this.mailProvider.sendMail({
             to: {
                 name: user.name,
@@ -39,10 +45,11 @@ class SendForgotPasswordEmailService {
             },
             subject: '[GoBarber] Recuperação de senha',
             templateData: {
-                template: 'Olá, {{name}}: {{token}}',
+                file: forgotPasswordTemplate,
                 variables: {
                     name: user.name,
                     token,
+                    link: `http://localhost:3000/reset_password?token=${token}`,
                 },
             },
         });

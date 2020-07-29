ea3ff64f649a057a8fcd16a74c1eafeed6d5c417
Leonardo Viana <sonleo2013@gmail.com>
2020-07-28 23:20:38 -0400

    created mail template with handlebars

--- backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,18 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import IMailProvider from '../models/IMailProvider';
+import { injectable, inject } from 'tsyringe';
 
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+
+@injectable()
 export default class EtheralMailProvider implements IMailProvider {
     private client: Transporter;
 
-    constructor() {
+    constructor(
+        @inject('MailTemplateProvider')
+        private mailTemplateProvider: IMailTemplateProvider,
+    ) {
         // criando conta de testes de e-mail
         nodemailer.createTestAccount().then(account => {
             const transporter = nodemailer.createTransport({
@@ -17,18 +25,27 @@ export default class EtheralMailProvider implements IMailProvider {
                 },
             });
 
-            console.log(account);
-
             this.client = transporter;
         });
     }
 
-    public async sendMail(to: string, body: string): Promise<void> {
+    public async sendMail({
+        to,
+        from,
+        subject,
+        templateData,
+    }: ISendMailDTO): Promise<void> {
         const message = await this.client.sendMail({
-            from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-            to,
-            subject: 'Recuperação de senha',
-            text: body,
+            from: {
+                name: from?.name || 'Equipe GoBarber',
+                address: from?.email || 'equipe@gobarber.com.br',
+            },
+            to: {
+                name: to.name,
+                address: to.email,
+            },
+            subject,
+            html: await this.mailTemplateProvider.parse(templateData),
         });
 
         console.log('Message sent: %s', message.messageId);

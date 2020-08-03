c54254fd9b3fef2cd9856b67c8288bf4f070909c
Leonardo Viana <sonleo2013@gmail.com>
2020-08-03 16:14:29 -0400

    removing older hours for providers availability

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -45,14 +45,19 @@ class ListProviderDayAvailabilityService {
             (_, index) => index + hourStart,
         );
 
+        const currentDate = new Date(Date.now());
+
         const availability = eachHourArray.map(hour => {
             const hasAppointmentInHour = appointments.find(
                 appointment => getHours(appointment.date) === hour,
             );
 
+            const compareDate = new Date(year, month - 1, day, hour);
+
             return {
                 hour,
-                available: !hasAppointmentInHour,
+                available:
+                    !hasAppointmentInHour && isAfter(compareDate, currentDate),
             };
         });
 

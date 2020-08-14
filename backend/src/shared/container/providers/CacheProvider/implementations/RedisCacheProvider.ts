db87a781ee0d56ce00ef559482dc81fe4ba93b46
Leonardo Viana <sonleo2013@gmail.com>
2020-08-13 22:13:26 -0400

    feat: invalidate key by prefix in cache

--- backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -26,4 +26,17 @@ export default class RedisCacheProvider implements ICacheProvider {
     }
 
     public async invalidate(key: string): Promise<void> {}
+
+    public async invalidatePrefix(prefix: string): Promise<void> {
+        const keys = await this.client.keys(`${prefix}:*`);
+
+        // for executing multiple executions at the same time
+        const pipeline = this.client.pipeline();
+
+        keys.forEach(key => {
+            pipeline.del(key);
+        });
+
+        await pipeline.exec();
+    }
 }

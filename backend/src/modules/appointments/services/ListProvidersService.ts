35ba23c676a18c5c39f4a59730c0a3c9326acb42
Leonardo Viana <sonleo2013@gmail.com>
2020-08-13 21:42:33 -0400

    feat: save providers list in cache

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,8 +1,10 @@
 /* eslint-disable camelcase */
 import { injectable, inject } from 'tsyringe';
 
-import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
     user_id: string;
@@ -13,12 +15,25 @@ class ListProvidersService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        const users = await this.usersRepository.findAllProviders({
-            except_user_id: user_id,
-        });
+        let users = await this.cacheProvider.recover<User[]>(
+            `providers-list:${user_id}`,
+        );
+
+        if (!users) {
+            users = await this.usersRepository.findAllProviders({
+                except_user_id: user_id,
+            });
+
+            console.log('a query no banco foi feita!');
+
+            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+        }
 
         return users;
     }

9c760a734dc62f1c44ca07737b88a3e8c0d09c0a
Leonardo Viana <sonleo2013@gmail.com>
2020-08-15 12:46:59 -0400

    test: use fake cache provider

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -3,11 +3,9 @@ import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
-import CreateUserService from './CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashProvider: FakeHashProvider;
-let createUser: CreateUserService;
 let authenticateUser: AuthenticateUserService;
 
 describe('AuthenticateUser', () => {
@@ -15,10 +13,6 @@ describe('AuthenticateUser', () => {
         fakeUsersRepository = new FakeUsersRepository();
         fakeHashProvider = new FakeHashProvider();
 
-        createUser = new CreateUserService(
-            fakeUsersRepository,
-            fakeHashProvider,
-        );
         authenticateUser = new AuthenticateUserService(
             fakeUsersRepository,
             fakeHashProvider,
@@ -26,7 +20,7 @@ describe('AuthenticateUser', () => {
     });
 
     it('should be able to authenticate', async () => {
-        const user = await createUser.execute({
+        const user = await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'johndoe@example.com',
             password: '12345',
@@ -51,7 +45,7 @@ describe('AuthenticateUser', () => {
     });
 
     it('should not be able to authenticate with wrong password', async () => {
-        await createUser.execute({
+        await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'johndoe@example.com',
             password: '12345',
